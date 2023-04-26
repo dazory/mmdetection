7afc2501db0e9b0d0f414ff573d92b607e22c930
dazory <dazory@naver.com>
2023-04-26 21:15:53 +0900

    add config files for multi-view

---
+++ configs/_dshong/multi_views_faster_rcnn_r50_fpn_1x_cityscapes.py
@@ -0,0 +1,75 @@
+_base_ = [
+    '../_base_/models/multi_view_faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/cityscapes_detection.py',
+    '../_base_/default_runtime.py'
+]
+num_views = 2
+
+### Model ###
+model = dict(
+    backbone=dict(init_cfg=None),
+    roi_head=dict(
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=8,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
+    num_views=num_views,
+)
+
+### Data ###
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='MultiViewAugmentation', num_views=num_views, use_clean=True),  # TODO: replace with your own augmentation
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='MultiViewDefaultFormatBundle'),  # NOTE: must be used for multi-view training
+    dict(type='MultiViewCollect',
+         fields_with_keys=dict(
+             img='img',
+             bbox='gt_bboxes',
+             label='gt_labels')),  # NOTE: must be used for multi-view training
+]
+data = dict(
+    samples_per_gpu=2,
+    train=dict(dataset=dict(pipeline=train_pipeline))
+)
+
+### Schedule ###
+# optimizer
+# lr is set for a batch size of 8
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    # [7] yields higher performance than [6]
+    step=[7])
+runner = dict(
+    type='EpochBasedRunner', max_epochs=8)  # actual epoch = 8 * 8 = 64
+log_config = dict(interval=100)
+# For better, more stable performance initialize from COCO
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'  # noqa
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (1 samples per GPU)
+auto_scale_lr = dict(base_batch_size=8)
