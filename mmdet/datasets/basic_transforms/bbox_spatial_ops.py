0cd2a9a007a2c8ae2a41ba78a29a84a4cdd90e5c
dazory <dazory@naver.com>
2023-05-01 21:04:24 +0900

    allow gaussian blured bbox

--- mmdet/datasets/basic_transforms/bbox_spatial_ops.py
+++ mmdet/datasets/basic_transforms/bbox_spatial_ops.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import kornia.filters as F
 import kornia.geometry.transform as T
 
 from ..builder import TRANSFORMATIONS
@@ -12,8 +13,16 @@ def sample_level(n):
 
 @TRANSFORMATIONS.register_module()
 class BboxRotate(Rotate):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, blur=None, *args, **kwargs):
         super(BboxRotate, self).__init__(*args, **kwargs)
+        if blur is not None:
+            assert isinstance(blur, dict), \
+                f"blur should be a dict, but got {type(blur)} with value {blur}"
+            self.blur = True
+            self.Kx, self.Ky = blur['Kx'], blur['Ky']
+            self.r = blur['r']
+        else:
+            self.blur = False
 
     def __call__(self, img, bboxes=None, **kwargs):
         if len(img.shape) == 3:
@@ -28,9 +37,17 @@ class BboxRotate(Rotate):
             masks = torch.zeros(
                 (num_objs, 1, img.shape[-2], img.shape[-1]),
                 device=img.get_device(), dtype=img.dtype)
+            gaussian_kernels = torch.zeros(
+                (num_objs, self.Kx, self.Ky), device=img.get_device(), dtype=img.dtype) \
+                if self.blur else None
             for j in range(num_objs):
                 x1, y1, x2, y2 = bbox[j].type(torch.int32)
                 masks[j, :, y1:y2, x1:x2] = 1
+                if self.blur:
+                    sigma_x, sigma_y = (x2 - x1) * self.r / 3.0, (y2 - y1) * self.r / 3.0
+                    gaussian_kernels[j] = F.get_gaussian_kernel2d((self.Kx, self.Ky), (sigma_x, sigma_y))[0]
+            if self.blur:
+                masks = F.filter2d(masks, gaussian_kernels)
 
             angles = self._get_angles(num_objs, device=img.get_device(), dtype=img.dtype)
             centers = torch.stack(((bbox[:, 0] + bbox[:, 2]) / 2,
@@ -46,8 +63,16 @@ class BboxRotate(Rotate):
 
 @TRANSFORMATIONS.register_module()
 class BboxShear(Shear):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, blur=None, *args, **kwargs):
         super(BboxShear, self).__init__(*args, **kwargs)
+        if blur is not None:
+            assert isinstance(blur, dict), \
+                f"blur should be a dict, but got {type(blur)} with value {blur}"
+            self.blur = True
+            self.Kx, self.Ky = blur['Kx'], blur['Ky']
+            self.r = blur['r']
+        else:
+            self.blur = False
 
     def __call__(self, img, bboxes=None, **kwargs):
         if len(img.shape) == 3:
@@ -62,9 +87,17 @@ class BboxShear(Shear):
             masks = torch.zeros(
                 (num_objs, 1, img.shape[-2], img.shape[-1]),
                 device=img.get_device(), dtype=img.dtype)
+            gaussian_kernels = torch.zeros(
+                (num_objs, self.Kx, self.Ky), device=img.get_device(), dtype=img.dtype) \
+                if self.blur else None
             for j in range(num_objs):
                 x1, y1, x2, y2 = bbox[j].type(torch.int32)
                 masks[j, :, y1:y2, x1:x2] = 1
+                if self.blur:
+                    sigma_x, sigma_y = (x2 - x1) * self.r / 3.0, (y2 - y1) * self.r / 3.0
+                    gaussian_kernels[j] = F.get_gaussian_kernel2d((self.Kx, self.Ky), (sigma_x, sigma_y))[0]
+            if self.blur:
+                masks = F.filter2d(masks, gaussian_kernels)
 
             x_degrees = self._get_shearing_degrees(num_objs, device=img.get_device())
             y_degrees = self._get_shearing_degrees(num_objs, device=img.get_device())
@@ -98,8 +131,16 @@ class BboxShearY(BboxShear):
 
 @TRANSFORMATIONS.register_module()
 class BboxTranslate(Translate):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, blur=None, *args, **kwargs):
         super(BboxTranslate, self).__init__(*args, **kwargs)
+        if blur is not None:
+            assert isinstance(blur, dict), \
+                f"blur should be a dict, but got {type(blur)} with value {blur}"
+            self.blur = True
+            self.Kx, self.Ky = blur['Kx'], blur['Ky']
+            self.r = blur['r']
+        else:
+            self.blur = False
 
     def __call__(self, img, bboxes=None, **kwargs):
         if len(img.shape) == 3:
@@ -114,6 +155,9 @@ class BboxTranslate(Translate):
             masks = torch.zeros(
                 (num_objs, 1, img.shape[-2], img.shape[-1]),
                 device=img.get_device(), dtype=img.dtype)
+            gaussian_kernels = torch.zeros(
+                (num_objs, self.Kx, self.Ky), device=img.get_device(), dtype=img.dtype) \
+                if self.blur else None
 
             translation_x, translation_y = [], []
             for j in range(num_objs):
@@ -122,6 +166,13 @@ class BboxTranslate(Translate):
 
                 translation_x.append(self._get_translation(x2 - x1))
                 translation_y.append(self._get_translation(y2 - y1))
+
+                if self.blur:
+                    sigma_x, sigma_y = (x2 - x1) * self.r / 3.0, (y2 - y1) * self.r / 3.0
+                    gaussian_kernels[j] = F.get_gaussian_kernel2d((self.Kx, self.Ky), (sigma_x, sigma_y))[0]
+
+            if self.blur:
+                masks = F.filter2d(masks, gaussian_kernels)
 
             translation_x = torch.tensor(translation_x, device=img.get_device())
             translation_y = torch.tensor(translation_y, device=img.get_device())
