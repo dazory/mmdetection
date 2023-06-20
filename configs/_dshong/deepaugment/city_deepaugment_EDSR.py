ee742f56c385e19974f434c9bf7407f56845c7d2
dazory <dazory@naver.com>
2023-06-20 15:26:33 +0900

    add configs for deepaugment

--- configs/_dshong/deepaugment/city_deepaugment_EDSR.py
+++ configs/_dshong/deepaugment/city_deepaugment_EDSR.py
@@ -5,10 +5,10 @@ _base_ = [
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', channel_order='rgb'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    # dict(type='RandomFlip', flip_ratio=0.5),
     # dict(type='DeepAugment', model_name='EDSR', save_mode=True), ## -> image
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -16,9 +16,9 @@ train_pipeline = [
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', channel_order='rgb'),
     dict(type='Resize', img_scale=[(2048, 800), (2048, 1024)], keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    # dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='DeepAugment', model_name='EDSR', save_mode=True, use_flip=False),  ## -> image
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
