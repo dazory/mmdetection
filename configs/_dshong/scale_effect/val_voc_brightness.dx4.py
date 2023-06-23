59c6dd151cc5c9f785ccf0b309838f16006c36bc
dazory <dazory@naver.com>
2023-06-23 15:04:42 +0900

    [val/log_dist_per_scale] add config for run log_dist_per_scale

---
+++ configs/_dshong/scale_effect/val_voc_brightness.dx4.py
@@ -0,0 +1,73 @@
+_base_ = ['../faster_rcnn_r50_fpn_1x_voc0712.py']
+
+num_views = 2
+dx = 0.4
+name = "voc-randcolor-faster_rcnn"
+
+WANDB_ENTITY = "kaist-url-ai28"
+WANDB_PROJECT_NAME = "mmdetection_oa"
+
+
+### Model ###
+model = dict(
+    type='NViewsFasterRCNN',
+    layer_name_type_list=[('roi_head.bbox_roi_extractor', 'input'),
+                          ('roi_head.bbox_roi_extractor', 'module'),
+                          ('roi_head.bbox_roi_extractor', 'output')],
+    do_something=dict(type='ScaleEffect', )
+)
+
+### Data ###
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Brightness', dx=dx, num_views=num_views),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels',
+                               'img2', 'dx2']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1000, 600),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='RandomColor', num_views=num_views),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img', 'img2']),
+            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels',
+                                       'img2', 'dx2']),
+        ])
+]
+dataset_type = 'VOCDataset'
+data = dict(
+    train=dict(dataset=dict(pipeline=train_pipeline)),
+    val=dict(type=dataset_type, pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline),
+)
+
+### Logger ###
+log_config = dict(
+    hooks=[
+        dict(type='CustomMMDetWandbHook',
+             init_kwargs={
+                 "entity": WANDB_ENTITY, "project": WANDB_PROJECT_NAME,
+                 "name": name,
+             },
+             interval=50,
+             log_checkpoint=True,
+             log_checkpoint_metadata=True,
+             num_eval_images=100,
+             bbox_score_thr=0.3)
+    ])
