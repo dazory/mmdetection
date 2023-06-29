28d25687e5b260cf1a70bde24d4a54a50ace2438
dazory <dazory@naver.com>
2023-06-29 14:45:16 +0900

    generate commands

--- tools/command_generator.py
+++ tools/command_generator.py
@@ -5,20 +5,20 @@ def get_command(type, seed, category, config_name):
     if type == 'train':
         comm = f'python3 -u /ws/external/tools/train.py ' \
                f'--seed {seed} ' \
-               f'--deterministic /ws/external/configs/_dshong/cityscapes/{category}/{config_name}.py ' \
-               f'--work-dir /ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}'
+               f'--deterministic /ws/external/configs/_dshong/{category}/{config_name}.py ' \
+               f'--work-dir /ws/data/dshong/_dshong/{category}/{folder_name}'
     elif type == 'test':
         comm = f"python3 -u /ws/external/tools/analysis_tools/test_robustness.py " \
-               f"/ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}/{config_name}.py " \
-               f"/ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}/epoch_2.pth " \
-               f"--out /ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}/epoch_2.pkl " \
+               f"/ws/data/dshong/_dshong/{category}/{folder_name}/{config_name}.py " \
+               f"/ws/data/dshong/_dshong/{category}/{folder_name}/epoch_2.pth " \
+               f"--out /ws/data/dshong/_dshong/{category}/{folder_name}/epoch_2.pkl " \
                f"--corruptions benchmark --eval bbox --load-dataset corrupted " \
-               f"| tee /ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}/test_robustness_result_2epoch.txt"
+               f"| tee /ws/data/dshong/_dshong/{category}/{folder_name}/test_robustness_result_2epoch.txt"
     elif type == 'parse':
         comm = f"python3 /ws/external/tools/analysis_tools/parse_txt2dict.py " \
-               f"/ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}/test_robustness_result_2epoch.txt " \
-               f"/ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}/{config_name}.py " \
-               f"| tee /ws/data/dshong/_dshong/cityscapes/{category}/{folder_name}/summary_result_2epoch.txt"
+               f"/ws/data/dshong/_dshong/{category}/{folder_name}/test_robustness_result_2epoch.txt " \
+               f"/ws/data/dshong/_dshong/{category}/{folder_name}/{config_name}.py " \
+               f"| tee /ws/data/dshong/_dshong/{category}/{folder_name}/summary_result_2epoch.txt"
     elif type == 'all':
         comm1 = get_command('train', seed, category, config_name)
         comm2 = get_command('test', seed, category, config_name)
